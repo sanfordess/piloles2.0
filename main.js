2a4c59492f6ba5b0ddf030608342822a9c311a05
sanfordess <60143610+sanfordess@users.noreply.github.com>
2020-04-28 00:38:24 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
- $('.nav-toggle').on('click', function() {
-      $('#menu').toggleClass('active');
-    });
+(function($) {
+  $(window).on("load", function() {
+  	$("a[rel='m_PageScroll2id']").mPageScroll2id({
+  		offset : 73
+  	});
+  });
+})(jQuery);
+$('.menu-btn').on('click', function() {
+    $('#menu').toggleClass('active');
+});
+$('.nav-btn').on('click', function() {
+    $('#menu').removeClass('active');
+    $('.menu-btn').removeClass('menu-btn_active');
+});
+ $('.menu-btn').on('click', function(e) {
+  e.preventDefault();
+  $(this).toggleClass('menu-btn_active');
+});
